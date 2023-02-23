1ac6d1c4f93aa361bb5e776032476fabd5ef83ff
amyjording <amyj@keystoneaccountability.org>
2023-02-23 09:03:22 -0800

    Add Signal to create Author upon User creation

--- thelema/settings.py
+++ thelema/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7_r*gv1l(kl_5d5f=iw_6-g40_-4rhwmti4vlzzqhee9ya&0w+'
+SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
